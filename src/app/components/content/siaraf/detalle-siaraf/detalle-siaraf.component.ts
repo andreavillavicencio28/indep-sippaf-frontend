5c50d39d7753ef731f8190c8222e133651384f59
MartinReyes22 <martinrs2201@icloud.com>
2023-12-01 23:54:15 -0600

    Avance de los modulos SIARAF y SAEOG

--- src/app/components/content/siaraf/detalle-siaraf/detalle-siaraf.component.ts
+++ src/app/components/content/siaraf/detalle-siaraf/detalle-siaraf.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+
 
 @Component({
   selector: 'sg-detalle-siaraf',
@@ -6,5 +9,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./detalle-siaraf.component.scss']
 })
 export class DetalleSiarafComponent {
+  @Input() tipo: number = 0;
+  @Input() id_solicitud: number = 0;
+  @Input() solicitante = '';
+
+  
+  datosSiraf: any[] = [];
+
+
+
+
+  tipoVista: boolean = true;
+  showCamvasPrincipal: boolean = false;
+
+
+  datosSiaraf: any[] = [
+    {
+      cliente: 'Martin Reyes',
+      sucursal: 'CDMX',
+      fecha: '05/08/2022',
+      capV: 'SI',
+      saldo: '$5,385.24'
+    },
+    {
+      cliente: 'Pedro Sereno',
+      sucursal: 'Cueramaro',
+      fecha: '25/04/2023',
+      capV: 'SI',
+      saldo: '$85,578.04'
+    },
+    {
+      cliente: 'Angel Costilla',
+      sucursal: 'San Pedro',
+      fecha: '15/08/2021',
+      capV: 'NO',
+      saldo: '$3,257.25'
+    }
+  ];
 
 }
--- src/app/components/content/siaraf/siaraf.component.ts
+++ src/app/components/content/siaraf/siaraf.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { ConfirmarModalService } from 'src/app/services/confirmar-modal/confirmar-modal.service';
+import { CONSTANTES } from 'src/app/shared/constantes/constantes';
 
 @Component({
   selector: 'sg-siaraf',
@@ -6,5 +10,125 @@ import { Component } from '@angular/core';
   styleUrls: ['./siaraf.component.scss']
 })
 export class SiarafComponent {
+  @Input() tipo: number = 0;
+  @Input() id_solicitud: number = 0;
+  @Input() solicitante = '';
 
+
+
+  accionDetalle: number = 1; // accion de detalle
+  documentoNombre: string = '';
+  showDetalle: boolean = false;
+  showCargaDocumento:boolean = false;
+  FormularioConfirma !: FormGroup;
+  showGenerarOficio: boolean = false;
+  showAgregarDocumento:  boolean = false;
+  prevDocumento: boolean = false;
+  prevAnexo: boolean = false;
+
+
+  constructor(
+    private confirmarModalService: ConfirmarModalService,
+    private fb: FormBuilder,
+    public toastrService: ToastrService
+  ) {
+    this.crearFormularioConFirma();
+    this.FormularioConfirma.controls['rfc'].disable();
+  }
+
+  
+
+  datosDocumentos: any[] = [
+    {
+      nombreDoc: 'Acta'
+    },
+    {
+      nombreDoc: 'CURP'
+    },
+    {
+      nombreDoc: 'Identificacion Oficial'
+    }
+  ];
+
+
+  cambioAccionDetalle(accion: number) {
+    this.accionDetalle = accion;
+  }
+
+  abrirPdf(documentoNombre: string = '') {
+    this.documentoNombre = documentoNombre;
+    this.showDetalle = true;
+  }
+
+  cargarDocumento(documentoNombre: string = '', accion: string) {
+    this.documentoNombre = documentoNombre
+    this.showCargaDocumento = true;
+  }
+
+  eliminarDocumento(nombreDoc: string = '') {
+    //VALIDAR CAMPO OBSERVACIONES NO ESTE VACIO
+    this.documentoNombre = nombreDoc;
+    let mensaje = `¿Estas seguro que quieres eliminar el documento ${this.documentoNombre}?`;
+    let titulomsm = `Se eliminó correctamente el ${this.documentoNombre}`;
+    this.confirmarModalService.abriraModal(mensaje).subscribe(result => {
+      if (result) {
+        // El usuario aceptó
+        //  this.cerrarCamvasObs();
+        //  this.respuestaCofirmarModal.emit(true);
+        this.toastrService.success(titulomsm);
+      } else {
+        // El usuario canceló
+        //this.valorRespuestaComfirmarModal.emit(false);
+      }
+
+    });
+
+  }
+
+  crearFormularioConFirma() {
+    this.FormularioConfirma = this.fb.group({
+      key: [
+        '',
+        [Validators.required]
+      ],
+      cer: [
+        '',
+        [Validators.required]
+      ],
+      contra: [
+        '',
+        [Validators.required]
+      ],
+      rfc: [
+        '',
+        [
+          Validators.required, Validators.pattern(CONSTANTES.EXP_RFC)
+        ]
+      ]
+    });
+  }
+
+  generarOficio() {
+    this.showGenerarOficio = true;
+  }
+
+
+  cerrarCanvas(tipo: string) {
+    switch (tipo) {
+      case 'oficio':
+        this.showGenerarOficio = false;
+        break;
+      case 'documento':
+        this.showAgregarDocumento = false;
+        break;    
+      case 'prevDocumento':
+        this.prevDocumento = false;
+        break;
+      case 'prevAnexo':
+        this.prevAnexo = false;
+        break;
+      default:
+        break;
+    }
+  }
 }
