ac410e65dd3f6217fc63f59586ead91601fab318
MartinReyes22 <martinrs2201@icloud.com>
2023-12-12 09:57:26 -0600

    Correccion del Menú

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -9,18 +9,12 @@ import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 })
 export class SidebarComponent {
   public menuItems: any[] = [ 
-    { label: 'Acreditados', icon: 'bi bi-person-workspace', route: '/content/acreditados'},
+    { label: 'Atenciones', icon: 'bi bi-person', route: '/content/atenciones'},
     { label: 'Propuestas de Pago', icon: 'bi bi-pass-fill', route: '/content/propuestas' },
     { label: 'Sesiones', icon: 'bi bi-calendar-check', route: '/content/sesiones'},
-    { label: 'Bitácora', icon: 'bi bi-journal-check', route: '/content/bitacora'},
-    { label: 'Catálogos', icon: 'bi bi-book', route: '/content/catalogos'},
-    { label: 'Admin', icon: 'bi bi-person-fill-gear', route: '/content/administracion'},
+    { label: 'Administración', icon: 'bi bi-person-fill-gear', route: '/content/administracion'},
     { label: 'Archivos', icon: 'bi bi-files', route: '/content/archivos'},
-    { label: 'Atenciones', icon: 'bi bi-person', route: '/content/atenciones'},
-    { label: 'Roles y perfiles', icon: 'bi bi-person-lock', route: '/content/rolesPerfiles'},
-    { label: 'Cancelación y Consulta de propuestas', icon: 'bi bi-x-octagon', route: '/content/atenciones'},
+    { label: 'Acreditados', icon: 'bi bi-person-workspace', route: '/content/acreditados'},
     { label: 'Reportes', icon: 'bi bi-file-earmark-spreadsheet', route: '/content/reportes'},
-    { label: 'Historial', icon: 'bi bi-clock-history', route: '/content/historial'},
-    { label: 'Administracion de Alertas', icon: 'bi bi-exclamation-octagon ', route: '/content/alertas'},
   ];
 }
