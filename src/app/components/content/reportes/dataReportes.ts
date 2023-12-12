b5b744fdadb875a076240e0ea2ace1ca91f7339b
Angel Armando Saldivar <ang.arm.saldivar@gmail.com>
2023-12-12 08:11:03 -0600

    Correcion de pantallas

---
+++ src/app/components/content/reportes/dataReportes.ts
@@ -0,0 +1,59 @@
+export const dataAcreditados = [{
+    idAcreditado: 1,
+    deudor: 'Juan Pérez',
+    numeroCliente: '123ABC',
+    encomienda: 'E1',
+    medioAtencion: 'Teléfono',
+    monto: '123,456.00',
+    observaciones: 'Ninguna'
+  },
+  {
+    idAcreditado: 34562,
+    deudor: 'Lorena Zepeda',
+    numeroCliente: '187AFC',
+    encomienda: 'E1',
+    medioAtencion: '	Correo Electrónico',
+    monto: '234, 876.00',
+    observaciones: 'Si, es observable'
+  }]
+
+  export const dataFinanciero = [{
+    idAcreditado: 1234,
+    idPropuesta: 123,
+    cartera: 'Objetivo',
+    fecha: '2020-12-08'   
+  },
+  {
+    idAcreditado: 7654,
+    idPropuesta: 765,
+    cartera: 'Control y Resguardo',
+    fecha: '2020-12-08'   
+  }]
+
+  export const dataCoper = [ {
+    idAcreditado: 7654,
+    idPropuesta: 765,
+    numeroSesion: 214,
+    estatus: 'Aceptada'   
+  },
+  {
+    idAcreditado: 7654,
+    idPropuesta: 765,
+    numeroSesion: 214,
+    estatus: 'En proceso'   
+  }]
+
+  export const dataSeguimientoCoper = [{
+    noSesion: 34567,
+    fechaAsignada: '21/10/2023',
+    fechaPresentacion: '25/10/2023',
+    canalSolucion: 'Canal 1',
+    fechaVencimiento: '31/12/2023'   
+  },
+  {
+    noSesion: 34567,
+    fechaAsignada: '21/10/2023',
+    fechaPresentacion: '25/10/2023',
+    canalSolucion: 'Canal 1',
+    fechaVencimiento: '31/12/2023'   
+  }]
--- src/app/components/content/reportes/reportes.component.ts
+++ src/app/components/content/reportes/reportes.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ConfirmarModalService } from 'src/app/services/confirmar-modal/confirmar-modal.service';
 import { ToastrService } from 'ngx-toastr';
+import { dataAcreditados, dataFinanciero, dataCoper, dataSeguimientoCoper } from './dataReportes';
 
 @Component({
   // imports: [NgbDropdownModule, AdminModule, CommonModule, FormsModule, ReactiveFormsModule, PdfViewerModule],
@@ -10,13 +11,28 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./reportes.component.scss'],
 })
 export class ReportesComponent {
-  tipoVista: boolean = false;
+  filtrosReportes: boolean = false;
   Seleccionado: number = 0;
   showEditPerfilRol: boolean = false;
   listaDatos: any[] = [];
   nombrePerfil: string = 'Hola';
   isDisplay = false;
   showTipoReporte: boolean = false;
+  filtroAtencion: boolean = false;
+  atencionAcred: boolean = false;
+  activosFin: boolean = false;
+  subtitulo: string = '';
+  dataAcreditados: any[] = [];
+  dataFinanciero: any[] = [];
+  dataCoper: any[] = [];
+  dataSeguimientoCoper: any[] = [];
+  tablaAtencionAcreditados: boolean = false;
+  tablaFinancieros: boolean = false;
+  tablaCoper: boolean = false;
+  tablaSeguimientoCoper: boolean = false;
+  opcion: number = 0;
+  btnExport: boolean = false;
+
 
   constructor(
     public toastrService: ToastrService
@@ -26,34 +42,42 @@ export class ReportesComponent {
 
   changeSubseccion() {}
 
-  cambioSeleccion(num: number) {
-    this.Seleccionado = num;
-    this.tipoVista = true;
+  cambioSeleccion() {
 
-    this.listaDatos = [{
-      id: 1, 
-      usuario: 'agonzalezj',
-      nombre: 'Alfonso Gonzalez J',
-      estatus: 'Activo',
-      estatusAD: 'Activo',
-      perfil: 'Administrador'
-    },
-    {
-      id: 1, 
-      usuario: 'xhernandexc',
-      nombre: 'Ximena Hernández C',
-      estatus: 'Activo',
-      estatusAD: 'Activo',
-      perfil: 'Coordinador'
-    },
-    {
-      id: 1, 
-      usuario: 'jjaimest',
-      nombre: 'Javier Jaimes T',
-      estatus: 'Inactivo',
-      estatusAD: 'Activo',
-      perfil: 'Especialista'
-    }]
+    this.btnExport = true;
+    
+    switch (this.opcion) {
+      case 1:
+        this.tablaAtencionAcreditados = true;
+        this.dataAcreditados = dataAcreditados;
+        this.tablaCoper = false;
+        this.tablaSeguimientoCoper = false;
+        this.tablaFinancieros = false;
+        break;
+      case 2:
+        this.tablaFinancieros = true;
+        this.tablaAtencionAcreditados = false;
+        this.tablaCoper = false;
+        this.tablaSeguimientoCoper = false;
+        this.dataFinanciero = dataFinanciero;
+        break;
+      case 3:
+        this.tablaFinancieros = false;
+        this.tablaAtencionAcreditados = false;
+        this.tablaCoper = true;
+        this.tablaSeguimientoCoper = false;
+        this.dataCoper = dataCoper;
+        break;
+      case 4:
+        this.tablaFinancieros = false;
+        this.tablaAtencionAcreditados = false;
+        this.tablaCoper = false;
+        this.tablaSeguimientoCoper = true;
+        this.dataSeguimientoCoper = dataSeguimientoCoper;
+        break;
+      default:
+        break;
+    }
   }
 
   editarRolPerfil(nombre: string) {
@@ -66,5 +90,37 @@ export class ReportesComponent {
   }
   cerrarCanvas() {
     this.showEditPerfilRol = false;
+  }
+  filtros(tipo:string) {
+    console.log("TIPO: " + tipo);
+    switch (tipo) {      
+      
+      case 'atencionAcred':
+        this.atencionAcred = true;
+        this.filtrosReportes = true;
+        this.subtitulo = 'Reporte atención de acreditados';
+        this.opcion = 1;
+        break;
+      case 'activosFin':
+        this.atencionAcred = false;  
+        this.filtrosReportes = true;
+        this.subtitulo = 'Propuestas de pago o compra de los acreditados o terceros relativas a activos financieros';
+        this.opcion = 2;
+      break;
+      case 'coper':
+        this.atencionAcred = true;
+        this.filtrosReportes = true;
+        this.subtitulo = 'Propuestas de pago sometidas y aprobadas por el COPER';
+        this.opcion = 3;
+        break;
+      case 'seguiCoper':
+        this.atencionAcred = true;
+        this.filtrosReportes = true;
+        this.subtitulo = 'Detalle de seguimiento de propuestas de pago o compra de los acreditados o terceros ante los órganos colegiados';
+        this.opcion = 4;
+        break;
+      default:
+        break;
+    }    
   }
 }
